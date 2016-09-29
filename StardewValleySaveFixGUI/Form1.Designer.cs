f7e1abd8239aa374ce1b083e4456e8c0fe2b8315
LeanZo <kashchannel@live.com>
2016-09-28 21:41:17 -0300

    Aplicativo Concluido
    Faz todas as funções da versão console porém com uma User Interface.

--- StardewValleySaveFixGUI/Form1.Designer.cs
+++ StardewValleySaveFixGUI/Form1.Designer.cs
@@ -28,31 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // listBox1
+            // button1
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(149, 117);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(86, 30);
-            this.listBox1.TabIndex = 2;
+            this.button1.Location = new System.Drawing.Point(47, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Corrigir!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Stardew Valley SaveFix";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Selecione um save:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(5, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "LeanZo e Kratscheky | Source: Draivin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(146, 1);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 7);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "3.0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(172, 116);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.ShowIcon = false;
+            this.Text = "SaveFix";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.ListBox listBox1;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- StardewValleySaveFixGUI/Form1.cs
+++ StardewValleySaveFixGUI/Form1.cs
@@ -8,18 +8,322 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using Newtonsoft.Json;
+using System.Xml.Linq;
 
 namespace StardewValleySaveFixGUI
 {
     public partial class Form1 : Form
     {
+        ComboBox comboBoxSaves;
         public Form1()
         {
             InitializeComponent();
-            ListBox listBox1 = new ListBox();
             MyClass obj = new MyClass();
-            listBox1.DataSource = obj.listaSaves;
-            Controls.Add(listBox1);
+            comboBoxSaves = new ComboBox();
+            comboBoxSaves.Location = new System.Drawing.Point(27, 47);
+            comboBoxSaves.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            int numeroSaves = obj.listaSaves.Length;
+            string lista = "undefined";
+            string savesDir2 = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "StardewValley\\Saves");
+            for (int i = 0; i < obj.listaSaves.Length; i++)
+            {
+                lista = obj.listaSaves[i];
+                lista = lista.Replace(savesDir2 + @"\", null);
+                obj.listaSaves[i] = lista;
+            }
+            comboBoxSaves.DataSource = obj.listaSaves;
+
+            this.Controls.Add(comboBoxSaves);
+        }
+
+
+        //SaveConv
+        static void ConvertSave(Dictionary<String, String> convertMap, string savenome)
+        {
+            var appDataDir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var savesDir = Path.Combine(appDataDir, "StardewValley\\Saves");
+
+            var save = Path.Combine(Path.Combine(savesDir, savenome), savenome);
+
+            File.Copy(save, Path.ChangeExtension(save, ".bak"), true);
+
+            XDocument doc = XDocument.Load(save);
+            var leaves =
+                from e in doc.Descendants()
+                where !e.Elements().Any()
+                select e;
+
+            foreach (var leaf in leaves)
+            {
+                var value = leaf.Value;
+                while (convertMap.ContainsKey(value))
+                {
+                    value = convertMap[value];
+                }
+
+                leaf.Value = value;
+            }
+
+            doc.Save(save);
+
+
+           // MessageBox.Show("Substituindo caracteres 0/2...");
+
+            string text = File.ReadAllText(save);
+            text = text.Replace('é', 'e');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('É', 'E');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('Á', 'A');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('á', 'a');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('À', 'A');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('à', 'a');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('Ã', 'A');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('ã', 'a');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('Â', 'A');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('â', 'a');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('Ê', 'E');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('ê', 'e');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('Í', 'I');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('í', 'i');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('Ó', 'O');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('ó', 'o');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('Ô', 'O');
+            File.WriteAllText(save, text);
+            Console.WriteLine("...");
+            text = File.ReadAllText(save);
+            text = text.Replace('ô', 'o');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('Õ', 'O');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('õ', 'o');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('Ú', 'U');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('ú', 'u');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('Ç', 'C');
+            File.WriteAllText(save, text);
+
+            text = File.ReadAllText(save);
+            text = text.Replace('ç', 'c');
+            File.WriteAllText(save, text);
+
+           // MessageBox.Show("Substituindo caracteres 1/2...");
+
+            var savegameinfodir = new FileInfo(save).Directory.FullName;
+            var savegameinfo = Path.Combine(savegameinfodir, "SaveGameInfo");
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('é', 'e');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('É', 'E');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('Á', 'A');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('á', 'a');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('À', 'A');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('à', 'a');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('Ã', 'A');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('ã', 'a');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('Â', 'A');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('â', 'a');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('Ê', 'E');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('ê', 'e');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('Í', 'I');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('í', 'i');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('Ó', 'O');
+            File.WriteAllText(savegameinfo, text);
+            Console.WriteLine("...");
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('ó', 'o');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('Ô', 'O');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('ô', 'o');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('Õ', 'O');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('õ', 'o');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('Ú', 'U');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('ú', 'u');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('Ç', 'C');
+            File.WriteAllText(savegameinfo, text);
+
+            text = File.ReadAllText(savegameinfo);
+            text = text.Replace('ç', 'c');
+            File.WriteAllText(savegameinfo, text);
+
+           // MessageBox.Show("Substituindo caracteres 2/2...");
+
+           // MessageBox.Show("Correção concluida");
+
+        } //Responsavel por realizar a conversao do save
+        //SaveConv
+
+
+
+        private void comboBoxSaves_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void button1_Click(object sender, System.EventArgs e)
+        {
+            int selected = comboBoxSaves.SelectedIndex;
+          //  MessageBox.Show(selected.ToString());
+            string current = (string)comboBoxSaves.SelectedItem;
+          //  MessageBox.Show(current);
+            //
+            // Main SaveConv
+            //
+            FileStream stream;
+
+            try
+            {
+                stream = new FileStream("SaveFix.json", FileMode.Open, FileAccess.Read);
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Arquivo SaveFix.json não foi encontrado");
+                return;
+            }
+
+            var reader = new StreamReader(stream);
+            var json = reader.ReadToEnd();
+            reader.Close();
+
+            Dictionary<string, string> conversions = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+
+            var running = true;
+            while (running)
+            {
+                ConvertSave(conversions, current);
+                MessageBox.Show("Correção Concluida! \n Você pode corrigir outro save.");
+                running = false;
+            }
+            //
+            // Main SaveConv
+            //
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
     public class MyClass
@@ -28,6 +332,7 @@ namespace StardewValleySaveFixGUI
         public string[] listaSaves = Directory.GetDirectories(savesDir);
         public MyClass()
         {
+
             foreach (var item in listaSaves)
             {
                 var item2 = item.Replace(savesDir + @"\", null);
